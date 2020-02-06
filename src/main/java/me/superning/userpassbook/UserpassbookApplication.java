d6a6ecd3be3594c362366ee9f1ea2fdbc3fd11c1
superning <xining.l.se@gmail.com>
2020-02-06 09:09:26 +0800

    用户模块第一次提交

---
+++ src/main/java/me/superning/userpassbook/UserpassbookApplication.java
@@ -0,0 +1,13 @@
+package me.superning.userpassbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserpassbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserpassbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/superning/userpassbook/UserpassbookApplicationTests.java
@@ -0,0 +1,13 @@
+package me.superning.userpassbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserpassbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
